3463e890c4e09e3f6bf810b00abbffcf3d9042a4
ggardusi <gustavo.gardusi@gmail.com>
2018-02-11 00:00:00 -0200

    "."

--- main.py
+++ main.py
@@ -13,22 +13,22 @@ def main():
   )
   with Committer() as committer:
     current_day = datetime.date(2018, 1, 27)
-    timeline = convertMessage(message = 'R')
+    timeline = convertMessage(message = 'H')
     for week in timeline:
       for weekday in week:
         current_day += datetime.timedelta(days = 1)
         if weekday != 1:
           continue
-        if current_day > datetime.date(2018, 2, 7):
+        if current_day != datetime.date(2018, 2, 11):
           continue
         committer.setDate(current_day)
-        for idx in range(2):
+        for idx in range(1):
           committer.gitAdd()
           committer.gitCommit()
-    # ~ print(committer.gitPush(
-      # ~ username = githubCredentials['username'],
-      # ~ password = githubCredentials['password'],
-      # ~ repositoryLink = 'https://github.com/WriteSomethingGood/Github-Pixel-Art-Tool'
-    # ~ ))
+    print(committer.gitPush(
+      username = githubCredentials['username'],
+      password = githubCredentials['password'],
+      repositoryLink = 'https://github.com/WriteSomethingGood/Github-Pixel-Art-Tool'
+    ))
 
 main()

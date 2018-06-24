f2eeee22dfa464ff41dde45b68c3da5589f5fc67
ggardusi <gustavo.gardusi@gmail.com>
2018-06-24 00:00:00 -0300

    "."

--- main.py
+++ main.py
@@ -12,15 +12,15 @@ def main():
     '/home/gardusi/github/credentials.json'
   )
   with Committer() as committer:
-    current_day = datetime.date(2018, 5, 5)
-    timeline = convertMessage(message = 'ME')
+    current_day = datetime.date(2018, 6, 23)
+    timeline = convertMessage(message = '!')
     for week in timeline:
       for weekday in week:
         current_day += datetime.timedelta(days = 1)
         if weekday != 1:
           continue
         committer.setDate(current_day)
-        for idx in range(100 - 76):
+        for idx in range(100):
           committer.gitAdd()
           committer.gitCommit()
     print(committer.gitPush(
